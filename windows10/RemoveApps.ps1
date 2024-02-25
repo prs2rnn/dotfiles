bc0f16390880882e59707b02c2f9a150f165a25f
prs2rnn <developer.prs2rnn@gmail.com>
2024-02-25 15:44:29 +0500

    Add RemoveApps.ps1 script for Windows 10

---
+++ windows10/RemoveApps.ps1
@@ -0,0 +1,31 @@
+# The apps I recommend to uninstall via copy/paste (you can copy/paste all at once):
+
+Get-AppxPackage -allusers Microsoft.BingWeather* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.GetHelp* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.MixedReality.Portal* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.Office.OneNote* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.People* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.Print3D* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.ScreenSketch* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.SkypeApp* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.StorePurchaseApp* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.Wallet* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsAlarms* | Remove-AppxPackage
+Get-AppxPackage -allusers microsoft.windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsFeedbackHub* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsMaps* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsSoundRecorder* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsStore* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.Xbox.TCUI* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.XboxApp* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.XboxGameOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.XboxGamingOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.XboxIdentityProvider* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.YourPhone* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.ZuneVideo* | Remove-AppxPackage
